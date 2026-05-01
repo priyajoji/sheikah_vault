16aafd1bf73a6b4008c1ee00784419dec2146b51
priyajoji <priyajoji212@gmail.com>
2026-05-01 20:43:38 +0530

    Initial setup: Spring Boot + PostgreSQL connected

---
+++ sheikahvault/src/main/java/com/sheikahvault/sheikahvault/SheikahvaultApplication.java
@@ -0,0 +1,13 @@
+package com.sheikahvault.sheikahvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheikahvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheikahvaultApplication.class, args);
+	}
+
+}
---
+++ sheikahvault/src/test/java/com/sheikahvault/sheikahvault/SheikahvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheikahvault.sheikahvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheikahvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
